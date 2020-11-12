0f604cd0b8ed5dc556a6d97dbf410a658c18d209
Rafael Jesus <rafael.jesus@soluis.com.br>
2020-11-12 00:46:41 -0300

    Initial Commit

---
+++ src/pages/ContactClient/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+
+export default function ContactClient() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{fontSize: 22}}>Clients</Text>
+    </View>
+  );
+};
